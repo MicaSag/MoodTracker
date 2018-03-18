e913be9f50810f501a1d518569b1139c4ab7210c
Michaël <sagotmickael@neuf.fr>
2018-03-18 15:50:44 +0100

    1_ Create a static key "KEY_POSITION"
    2_ Create method "newInstance" in PageFragment and add data "KEY_POSITION" to its bundle

--- app/src/main/java/com/happiness/ministry/moodtracker/Controllers/Fragments/PageFragment.java
+++ app/src/main/java/com/happiness/ministry/moodtracker/Controllers/Fragments/PageFragment.java
@@ -14,11 +14,27 @@ import com.happiness.ministry.moodtracker.R;
  */
 public class PageFragment extends Fragment {
 
+    // 1 - Create keys for our Bundle
+    private static final String KEY_POSITION="position";
 
     public PageFragment() {
         // Required empty public constructor
     }
 
+    // 2 - Method that will create a new instance of PageFragment, and add data to its bundle.
+    public static PageFragment newInstance(int position) {
+
+        // 2.1 Create new fragment
+        PageFragment frag = new PageFragment();
+
+        // 2.2 Create bundle and add it some data
+        Bundle args = new Bundle();
+        args.putInt(KEY_POSITION, position);
+
+        frag.setArguments(args);
+
+        return(frag);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
